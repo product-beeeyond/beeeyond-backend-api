66f6da3b6acebae27dce5ead1618b2329373e28b
apple <apple@Afiolabis-MacBook-Air.local>
2025-09-23 12:52:50 +0100

    rebase contd.

--- src/routes/multisig.ts
+++ src/routes/multisig.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // /* eslint-disable @typescript-eslint/no-explicit-any */
 // // // src/routes/multisig.ts
 // // import { Router } from 'express';
